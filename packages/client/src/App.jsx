c66bcfd4516ff0f28b553a897dcb1cfef167d4ac
tranthaison1231 <thaison.coderstokyo@gmail.com>
2021-10-22 14:51:30 +0700

    fix ui

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
-import { Button } from "antd";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/home";
-import ProjectDetail from "./pages/project-detail";
-import CreateProject from "./pages/create-project";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import "./components/top-navigation";
-import "./components/page-loader";
-import "./pages/dapp";
-import "./App.css";
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
+import ProjectDetail from './pages/project-detail';
+import CreateProject from './pages/create-project';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './components/top-navigation';
+import './components/page-loader';
+import './pages/dapp';
+import './App.css';
+
 class App extends Component {
   render() {
     return (
@@ -20,11 +21,9 @@ class App extends Component {
             {/* <page-loader id="page-loader" /> */}
             <Header />
             <div className="flex-grow">
-              <Route path="/" component={Home} exact/>
+              <Route path="/" component={Home} exact />
               <Route path="/project-detail" component={ProjectDetail} exact />
               <Route path="/create-project" component={CreateProject} exact />
-              {/* <ProjectDetail /> */}
-              {/* <CreateProject /> */}
             </div>
             <Footer />
           </div>
