124e3c778cdbfed2df0986f4378e759d0303688e
Long Nguyen <ntlong870@gmail.com>
2021-10-29 15:55:29 +0700

    loading skeleton

--- packages/client/src/components/ProjectDetails/ProjectDetail.js
+++ packages/client/src/components/ProjectDetails/ProjectDetail.js
@@ -1,13 +1,14 @@
-import { Avatar, Button, Input, Progress, notification, Skeleton } from 'antd';
-import React, { useEffect, useState } from 'react';
-import creator from '../../assets/dappstarter.png';
-import { fundProject, getRemainingTokenCount } from '../../flow/flow';
-import { useUserContext } from '../../providers/UserProvider';
-import { getIpfs } from '../../utils/ipfs';
-import axios from 'axios';
+import { Avatar, Button, Input, Progress, notification } from "antd";
+import React, { useEffect, useState } from "react";
+import creator from "../../assets/dappstarter.png";
+import { fundProject, getRemainingTokenCount } from "../../flow/flow";
+import { useUserContext } from "../../providers/UserProvider";
+import { getIpfs } from "../../utils/ipfs";
+import SkeletonLoading from "./SkeletonLoading";
+import axios from "axios";
 
 const ProjectDetail = ({ project }) => {
-  const [tokenCount, setTokenCount] = useState('');
+  const [tokenCount, setTokenCount] = useState("");
   const [inputError, setInputError] = useState(true);
   const { user, loggedIn, tools } = useUserContext();
   const [projectDetail, setProjectDetail] = useState(project);
@@ -22,7 +23,7 @@ const ProjectDetail = ({ project }) => {
           tokenCount: Number(tokenCount),
         };
         await fundProject(fundProjectData);
-        notification.success({ message: 'Funded project success!' });
+        notification.success({ message: "Funded project success!" });
       } catch (err) {
         console.error(err);
       }
@@ -38,7 +39,11 @@ const ProjectDetail = ({ project }) => {
           axios.get(projectIpfsUrl),
           getRemainingTokenCount(Number(project.projectId)),
         ]);
-        setProjectDetail((project) => ({ ...project, ...data, remainningToken: remainningToken }));
+        setProjectDetail((project) => ({
+          ...project,
+          ...data,
+          remainningToken: remainningToken,
+        }));
       } catch (err) {
         console.error(err);
       } finally {
@@ -49,7 +54,8 @@ const ProjectDetail = ({ project }) => {
   }, [project?.ipfsHash]);
 
   useEffect(() => {
-    if (typeof tokenCount !== 'number' && Number(tokenCount) <= 0) setInputError(true);
+    if (typeof tokenCount !== "number" && Number(tokenCount) <= 0)
+      setInputError(true);
     else setInputError(false);
   }, [tokenCount]);
 
@@ -58,12 +64,18 @@ const ProjectDetail = ({ project }) => {
   return (
     <div className="flex flex-row justify-around p-32">
       {loading ? (
-        <Skeleton />
+        <SkeletonLoading />
       ) : (
         <>
-          <img src={projectDetail?.imageURL} className="object-fit rounded-lg w-2/5" style={{ height: '45rem' }} />
+          <img
+            src={projectDetail?.imageURL}
+            className="object-fit rounded-lg w-2/5"
+            style={{ height: "45rem" }}
+          />
           <div className="flex flex-col max-w-md">
-            <h1 className="font-extrabold text-5xl">{projectDetail?.projectName}</h1>
+            <h1 className="font-extrabold text-5xl">
+              {projectDetail?.projectName}
+            </h1>
             <div className="flex flex-row justify-between">
               <div className="flex flex-row align-center items-center mr-12">
                 <Avatar src={projectDetail?.userImage || creator} size={40} />
@@ -82,20 +94,28 @@ const ProjectDetail = ({ project }) => {
             </div>
             <div className="flex flex-col rounded-lg border-gray-500 border-2 p-8 mt-8 flex-wrap max-w-md">
               <p className="text-xl mb-0">
-                <span className="font-bold mr-2">{projectDetail?.tokenPrice}</span>
+                <span className="font-bold mr-2">
+                  {projectDetail?.tokenPrice}
+                </span>
                 <span className="text-gray-1000">FLOW Coin</span>
               </p>
               <Progress percent={50} strokeColor="#00C48C" showInfo={false} />
-              <p className="text-gray-1000 text-base">{projectDetail?.projectDescription}</p>
+              <p className="text-gray-1000 text-base">
+                {projectDetail?.projectDescription}
+              </p>
             </div>
             <div className="flex flex-row justify-between content-center mt-8">
               <div className="flex justify-start flex-col items-center w-24">
                 <h1 className="font-extrabold text-2xl mb-0">0</h1>
-                <span className="text-xl text-center text-gray-700">Funder</span>
+                <span className="text-xl text-center text-gray-700">
+                  Funder
+                </span>
               </div>
               <div className="flex justify-start flex-col items-center w-24">
                 <h1 className="font-extrabold text-2xl mb-0">{`${projectDetail?.profitSharePercent}%`}</h1>
-                <span className="text-xl text-center text-gray-700">Profit of supporter</span>
+                <span className="text-xl text-center text-gray-700">
+                  Profit of supporter
+                </span>
               </div>
             </div>
             <div className="flex flex-col my-10">
@@ -107,16 +127,28 @@ const ProjectDetail = ({ project }) => {
                     onChange={(e) => {
                       const reg = /^[0-9\b]+$/;
                       const value = e.target.value;
-                      if (value.match(reg) || value === '') setTokenCount(e.target.value);
+                      if (value.match(reg) || value === "")
+                        setTokenCount(e.target.value);
                     }}
                     value={tokenCount}
                   />
-                  <Button className="mb-4 mt-4" type="primary" onClick={onSubmit} size="large" disabled={inputError}>
+                  <Button
+                    className="mb-4 mt-4"
+                    type="primary"
+                    onClick={onSubmit}
+                    size="large"
+                    disabled={inputError}
+                  >
                     Fund Project
                   </Button>
                 </>
               ) : (
-                <Button className="mt-4" type="primary" onClick={() => tools?.logIn()} size="large">
+                <Button
+                  className="mt-4"
+                  type="primary"
+                  onClick={() => tools?.logIn()}
+                  size="large"
+                >
                   Sign in to fund project
                 </Button>
               )}
---
+++ packages/client/src/components/ProjectDetails/SkeletonLoading.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const SkeletonLoading = () => {
+  return (
+    <>
+      <ContentLoader
+        speed={2}
+        width={567}
+        height={768}
+        viewBox="0 0 567 768"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+      >
+        <rect x="394" y="170" rx="0" ry="0" width="0" height="1" />
+        <rect x="30" y="1" rx="0" ry="0" width="567" height="788" />
+      </ContentLoader>
+      <ContentLoader
+        speed={2}
+        width={567}
+        height={768}
+        viewBox="0 0 567 768"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+      >
+        <rect x="394" y="170" rx="0" ry="0" width="0" height="1" />
+        <rect x="38" y="29" rx="0" ry="0" width="522" height="43" />
+        <circle cx="89" cy="160" r="66" />
+        <rect x="160" y="108" rx="0" ry="0" width="91" height="21" />
+        <rect x="161" y="140" rx="0" ry="0" width="122" height="21" />
+        <rect x="30" y="240" rx="0" ry="0" width="517" height="389" />
+        <rect x="257" y="348" rx="0" ry="0" width="1" height="13" />
+        <rect x="30" y="675" rx="0" ry="0" width="516" height="32" />
+        <rect x="28" y="726" rx="0" ry="0" width="516" height="30" />
+        <circle cx="360" cy="160" r="66" />
+        <rect x="431" y="108" rx="0" ry="0" width="91" height="21" />
+        <rect x="432" y="140" rx="0" ry="0" width="122" height="21" />
+      </ContentLoader>
+    </>
+  );
+};
+
+export default SkeletonLoading;
